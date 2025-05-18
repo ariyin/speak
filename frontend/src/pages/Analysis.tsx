a390fbce71c8acb509174eca47e909b17c4d62e7
Jenna Wang <jenna2004@gmail.com>
2025-05-18 16:49:15 -0700

    clean up analysis, set up summary

--- frontend/src/pages/Analysis.tsx
+++ frontend/src/pages/Analysis.tsx
@@ -19,30 +19,31 @@ function Analysis() {
   };
 
   return (
-    <>
-      <h1>Analysis</h1>
+    <div className="layout-tb">
+      <NavLink to="/" className="justify-self-end">
+        <button>exit</button>
+      </NavLink>
+      <div className="grid h-full grid-cols-2 gap-4">
+        <div>
+          {/* Jump to Middle button is here, outside of VideoPlayer */}
+          <button onClick={jumpToMiddle}>Jump to Middle</button>
 
-      {/* Jump to Middle button is here, outside of VideoPlayer */}
-      <button
-        onClick={jumpToMiddle}
-        className="mb-4 rounded bg-blue-600 px-4 py-2 text-white"
-      >
-        Jump to Middle
-      </button>
-
-      {/* Video Player */}
-      <VideoPlayer onReady={handleReady} />
-
-      {/* Navigation */}
-      <div className="mt-4 flex flex-col space-y-2">
-        <NavLink to="/type" className="text-blue-500 hover:underline">
-          Rehearse again
+          {/* Video Player */}
+          <VideoPlayer onReady={handleReady} />
+        </div>
+        <div className="rounded-2xl border-2 border-black p-4">
+          analysis goes here
+        </div>
+      </div>
+      <div className="flex justify-end gap-4">
+        <NavLink to="/type">
+          <button>rehearse again</button>
         </NavLink>
-        <NavLink to="/" className="text-red-500 hover:underline">
-          Exit
+        <NavLink to="/summary">
+          <button>finish</button>
         </NavLink>
       </div>
-    </>
+    </div>
   );
 }
 
